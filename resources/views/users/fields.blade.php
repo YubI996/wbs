90bafed68b74387ced0285377a697cb3cfe25e5e
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2021-05-17 11:58:15 +0800

    tambah field username

--- resources/views/users/fields.blade.php
+++ resources/views/users/fields.blade.php
@@ -6,6 +6,12 @@
 
 <!-- Name Field -->
 <div class="form-group col-sm-6">
+    {!! Form::label('username', 'Username:') !!}
+    {!! Form::text('username', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255,'maxlength' => 255]) !!}
+</div>
+
+<!-- Name Field -->
+<div class="form-group col-sm-6">
     {!! Form::label('name', 'Name:') !!}
     {!! Form::text('name', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255,'maxlength' => 255]) !!}
 </div>
