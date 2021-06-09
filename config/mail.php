b77f0bce2c63911e7beb15acebae2eae3749dc9c
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2021-06-09 13:33:25 +0800

    tambah driver smtp

--- config/mail.php
+++ config/mail.php
@@ -36,6 +36,7 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
+            'driver' => env('MAIL_DRIVER', 'smtp'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
@@ -85,7 +86,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('MAIL_FROM_NAME', 'WBS'),
     ],
 
     /*
